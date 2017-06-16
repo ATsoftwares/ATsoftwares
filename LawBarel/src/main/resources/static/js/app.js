7ba9a85ad40442a8ee84b0c5651811e55b6cb942
Aharon <aharon.b@openlegacy.com>
2017-06-16 16:45:01 +0300

    refactor of LawBarel home page
    organize code in seperate files and folder
    and all new look for home-page

---
+++ LawBarel/src/main/resources/static/js/app.js
@@ -0,0 +1,52 @@
+(function() {
+
+	'use strict';
+
+	/* App Module */
+
+	var app = angular.module("app", [ 'controllers', 'directives', 'ui.router']).run(
+			[ '$rootScope', '$state', function($rootScope, $state) {
+
+				$rootScope.check = "rootScope is checkd !";
+
+			} ]);
+
+	app.config(function($stateProvider, $urlRouterProvider) {
+
+		$urlRouterProvider.otherwise("/home");
+
+		var header = {
+			templateUrl : "views/header.html",
+			controller : "headerCntrl"
+		};
+
+		var footer = {
+			templateUrl : "views/footer.html",
+			controller : "footerCntrl"
+		};
+
+		addRout('home', '/home', 'homebody');
+
+		addRout('about', '/about', 'about');
+
+		addRout('activities', '/activities', 'activities');
+
+		addRout('contact', '/contact', 'contact');
+
+		function addRout(state, url, template) {
+			$stateProvider.state(state, {
+				url : url,
+				views : {
+					"header" : header,
+					"footer" : footer,
+					"" : {
+						templateUrl : "views/" + template + ".html",
+						controller : template + "Cntrl"
+					}
+				}
+			});
+		}
+		;
+	});
+
+})();
